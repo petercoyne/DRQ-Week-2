9429be7df29717648f3921fa4a4bd370dd1ac233
Peter Coyne <g00266507@gmit.ie>
2021-09-27 12:10:17 +0100

    exercise 2

---
+++ bmi.js
@@ -0,0 +1,15 @@
+class BMI { // create class BMI
+	
+	constructor (height, weight) { // constructor takes 2 arguments
+		this.weight = weight; // create local instances
+		this.height = height;
+	}
+
+	calculateBMI() { // add method
+		return this.weight / (this.height ** 2);
+	}
+}
+
+let MyBmi = new BMI(1.8,100); // create object
+
+console.log(MyBmi.calculateBMI()); // output returned calculation to console
